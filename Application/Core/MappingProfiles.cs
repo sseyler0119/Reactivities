e87cf46e81850ad4ee569d7fd166aad587a808f2
Serena Seyler <sseyler0119@gmail.com>
2023-12-06 11:33:38 -0800

    Complete Event Activities Tab

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,11 +1,12 @@
 using Application.Activities;
 using Application.Comments;
+using Application.Profiles;
 using AutoMapper;
 using Domain;
 
 namespace Application.Core
 {
-    public class MappingProfiles : Profile
+    public class MappingProfiles : AutoMapper.Profile
     {
         /* where we want to go from and where we want to go to*/
         public MappingProfiles()
@@ -42,6 +43,13 @@ namespace Application.Core
                 .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName))
                 .ForMember(d => d.Image, o => o.MapFrom(s => s.Author.Photos
                     .FirstOrDefault(x => x.IsMain).Url));
+
+            CreateMap<ActivityAttendee, UserActivityDto>()
+                .ForMember(d => d.Id, o => o.MapFrom(s => s.Activity.Id))
+                .ForMember(d => d.Date, o => o.MapFrom(s => s.Activity.Date))
+                .ForMember(d => d.Title, o => o.MapFrom(s => s.Activity.Title))
+                .ForMember(d => d.Category, o => o.MapFrom(s => s.Activity.Category))
+                .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Activity.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
         }        
     }
 }
